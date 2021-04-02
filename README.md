3f6828b5af68c0f7272ea12da963cbf1bb71edbc
Mary Mohlenkamp <marymkamp@aol.com>
2021-04-01 23:56:30 -0400

    All files updated

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # KimLawless-Menz-Website
 This project is to help promote my artist friend collection of artwork and prepare it for e-commerce
-There are three categories of artwork which are highlighted on the website.  The navigation bar is utilized to bring attention to these categories and JavaScript is being used to filter these areas.
+There are three categories of artwork which are highlighted on the website.  The navigation bar is utilized to bring attention to these categories and JavaScript is being used to hide the artist picture in mobile view.
--- js/script.js
+++ js/script.js
@@ -1,47 +1,10 @@
-filterSelection("all") // Execute the function and show all columns
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
-  }
-}
 
-// Show filtered elements
-function AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+@media (max-width: 480px) {
 
-// Hide elements that are not selected
-function RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1); 
-    }
-  }
-  element.className = arr1.join(" ");
-}
+document.getElementById("artist_picture").style.display = "none";
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+<script>
+  //Hiding the artist image in mobile view
+document.getElementByID(artist_picture).style.display = "none";
+</script>
 }
